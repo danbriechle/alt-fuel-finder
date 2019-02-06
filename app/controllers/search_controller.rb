59fd5f8cf5f2938baf7ac60a834bdfaac4106e92
Daniel Briechle <danbriechle@hotmail.com>
2019-02-06 10:44:23 -0700

    adds view logic

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,5 @@
 class SearchController < ApplicationController
   def index
     @stations = StationFacade.new(params["q"]).stations
-    binding.pry
   end
 end
--- spec/features/user_can_search_for_stations_spec.rb
+++ spec/features/user_can_search_for_stations_spec.rb
@@ -10,7 +10,16 @@ feature "User visits root" do
     # And I click "Locate"
     expect(current_path).to eq(search_path)
     # Then I should be on page "/search"
+    save_and_open_page
     expect(page).to have_content("10 Closest Stations")
+    within ".stations-UDR" do
+      expect(page).to have_content("Name: UDR")
+      expect(page).to have_content("Access Times: 24 hours daily")
+      expect(page).to have_content("Address: 800 Acoma St")
+      expect(page).to have_content("Distance: .31422 Miles")
+      expect(page).to have_content("Fuel Type: ELEC")
+    end
+
     # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
     # And the stations should be limited to Electric and Propane
     # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
